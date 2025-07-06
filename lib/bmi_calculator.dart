b16e1af969302dbd697ec90ec982d8ec55a0aad2
Minokainduwara <minokainduwara@gmail.com>
2025-07-06 13:51:18 +0530

    Project BMI

---
+++ lib/bmi_calculator.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class BMICalculatorPage extends StatefulWidget {
+  const BMICalculatorPage({super.key});
+
+  @override
+  State<BMICalculatorPage> createState() => _BMICalculatorPageState();
+}
+
+class _BMICalculatorPageState extends State<BMICalculatorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('BMI Calculator'),
+      ),
+      body: Container(
+        color: Colors.blue[50],
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+         children:[
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(onPressed: () {}, child: Text("Male")),
+                  ElevatedButton(onPressed: () {}, child: Text("FeMale")),
+                  ],
+              ),
+              Column(
+                children:[
+                  Text("Height"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("185", style: TextStyle(
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                        ),
+                        ),
+                      Text("cm"),
+                    ],
+                  ),
+                  Slider(
+                    min: 80,
+                    max: 220,
+                    value: 183,
+                    onChanged: (value) {},
+                    )
+                ]
+              ),
+              Column(
+                children: [
+                  Text("Weight"),
+                  Text("74",style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                  )
+                  ),
+                  //Text("kg"),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FloatingActionButton(
+                        elevation: 0,
+                        shape: ShapeBorder.lerp(
+                          CircleBorder(),
+                          CircleBorder(),
+                          0.5,
+                        ),
+                        onPressed: (){},
+                        child: Icon(Icons.remove),
+                      ),
+
+                      FloatingActionButton(
+                        elevation: 0,
+                        shape: ShapeBorder.lerp(
+                          CircleBorder(),
+                          CircleBorder(),
+                          0.5,
+                        ),
+                        onPressed: (){},
+                        child: Icon(Icons.add),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+          ElevatedButton(onPressed: () {}, child: Text("Calculate BMI")),
+         ],
+        ),
+      ),
+    );
+  }
+}
